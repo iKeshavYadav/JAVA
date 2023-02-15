23a1c52139873c5b29d7aace1b76188fb9a02eba
Keshav Yadav <Contact.KeshavYadav@gmail.com>
2023-02-15 19:33:08 +0530

    // # Variables in JAVA

---
+++ Hello.java
@@ -0,0 +1,39 @@
+// # Variables in JAVA
+
+/*
+class Hello
+{
+    public static void main(String a[])
+    {
+        int num1 = 3;
+        int num2 = 4;
+        int result = num1 + num2;
+        // System.out.println(result);
+    }
+}
+*/
+
+
+// # Data Types in JAVA (https://www.javatpoint.com/java-data-types)
+
+// 1. Primitive data types: boolean, char, byte, short, int, long, float and double.
+// 2. Non-primitive data types: String, Classes, Object, Interfaces, and Arrays.
+
+/*
+class Hello
+{
+    public static void main(String a[])
+    {
+        int num1 = 9; //4 bytes | Stores whole numbers from -2,147,483,648 to 2,147,483,647
+        byte by = 127; // 1 byte | Stores whole numbers from -128 to 127
+        short sh = 558; // 2 bytes | Stores whole numbers from -32,768 to 32,767
+        long lo = 5854l; // 8 bytes | Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+        float f = 5.8f; // 4 bytes | Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+        double d = 5.8; // 8 bytes | Stores fractional numbers. Sufficient for storing 15 decimal digits
+        
+        char c = 'K'; // 2 bytes | Stores a single character/letter or ASCII values | excepts only single quote
+        boolean b = true; // 1 bit | Stores true or false values
+    }
+}
+*/
