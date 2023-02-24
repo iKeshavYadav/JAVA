54a15189585feafc5ba57831cb1607797350fe5c
Keshav <contact.keshavyadav@gmail.com>
2023-02-24 18:52:51 +0530

    // Ternary operators in JAVA

--- Hello.java
+++ Hello.java
@@ -180,3 +180,27 @@ class Hello
     }
 }
 */
+
+// Ternary operators in JAVA
+/*
+class Hello
+{
+    public static void main(String[] args)
+    {
+        int n = 4;
+        int result = 0;
+
+        // if(n%2==0){
+        //     result = 10;
+        // }else{
+        //     result = 20;
+        // }
+        // System.out.println(result);
+
+
+        // ?: Ternary Operator
+        result = n%2==0 ? 10 : 20;
+        System.out.println(result);
+    }
+}
+*/
