91aa70483c159fbec963804a5bd484a5cebac906
Keshav <contact.keshavyadav@gmail.com>
2023-02-24 19:59:29 +0530

    // While Loops in JAVA

--- Hello.java
+++ Hello.java
@@ -240,3 +240,38 @@ class Hello {
     }
 }
 */
+
+
+// Loops - while, do while, for
+
+
+// While Loops in JAVA
+/*
+class Hello {
+    public static void main(String[] args) {
+        
+        // int i = 1;
+        // while(i <= 4){
+        //     System.out.println("Hi " + i);
+        //     i++;
+        // }
+        // System.out.println("Bye! " + i);
+        
+        
+        // # Nested while loop
+        int i = 1;
+        while(i <= 4){
+            System.out.println("Hi " + i);
+            
+            int j = 1;
+            while(j <= 3){
+                System.out.println("Hello" + j);
+                j++;
+            }
+            
+            i++;
+        }
+        System.out.println("Bye! " + i);
+    }
+}
+*/
