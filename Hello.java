e75be5ad8862e6adb51e534e5fbbb1256f192135
Keshav <contact.keshavyadav@gmail.com>
2023-02-25 21:05:38 +0530

    // Methods in JAVA

--- Hello.java
+++ Hello.java
@@ -331,3 +331,34 @@ class Hello {
     }
 }
 */
+
+// Methods in JAVA
+
+/*
+class Computer{
+    
+    public void playMusic(){
+        System.out.println("Music Playing...");
+    }
+    
+    public String getMeAPen(int cost){
+        if(cost >= 10){
+            return "Pen";
+        }else{
+            return "Nothing";
+        }
+    }
+}
+
+class Hello{
+    public static void main(String[] args){
+        
+        Computer obj = new Computer();
+        obj.playMusic(); // Music Playing...
+        
+        String str = obj.getMeAPen(12);
+        System.out.println(str); // Pen
+        
+    }
+}
+*/
