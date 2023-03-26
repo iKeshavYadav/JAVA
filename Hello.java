ea76e8dac848fe874a59ce3dac0b932c9d463c2f
Keshav <contact.keshavyadav@gmail.com>
2023-03-27 00:54:38 +0530

    // Static Method in JAVA

--- Hello.java
+++ Hello.java
@@ -648,3 +648,42 @@ class Hello {
 }
 */
 
+// Static Method in JAVA
+/*
+class Mobile{
+    String brand;
+    int price;
+    static String name;
+    
+    public void show(){
+        System.out.println(brand + " : " + price + " : " + name);
+    }
+    
+    // Static Method
+    public static void show1(Mobile obj){
+        System.out.println(obj.brand + " : " + obj.price + " : " + name);
+    }
+}
+
+class Hello {
+    public static void main(String[] args) {
+        
+        Mobile obj1 = new Mobile();
+        obj1.brand = "Apple";
+        obj1.price = 1500;
+        Mobile.name = "Smartphone";
+        
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Samsung";
+        obj2.price = 1700;
+        Mobile.name = "Phone";
+        
+        // obj1.show();
+        // obj2.show();
+        
+        // Calling static method directly with the help of class name & the obj which are refering to
+        Mobile.show1(obj1);
+    }
+}
+*/
+
